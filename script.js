965a06ecd714c4fd9c3e08f1a5eca7c53b5afd0f
Art <artem1324@gmail.com>
2023-09-15 23:30:43 -0700

    Add different color mode options
    This will add three different color mode options.
    Black - solid back color.
    Rainbow - solid rainbow colors.
    Rainbow with opacity change - starts off with 0%
    opacity and increases by 10% each time you hover over
    the sketch pad.

--- script.js
+++ script.js
@@ -12,13 +12,30 @@ function newRow() {
 }
 
 function newGrid(grid) {
-    console.log(gridSize);
     for (let i = 0; i < gridSize; i++) grid.appendChild(newRow());
 }
 
 function changeBackgroundColor() {
     const squares = document.querySelectorAll('.square');
-    squares.forEach(square => square.addEventListener("mouseover", () => square.classList.add("hover")));
+    squares.forEach(square => square.addEventListener("mouseover", function() {
+            if (colorMode === "black"){
+                square.style.backgroundColor = "black";
+            } else if (colorMode === "rainbow") {
+                square.style.backgroundColor = getRandomColor(1);
+            } else {
+                rainbowColor(square);
+            }
+    }));
+}
+
+function rainbowColor(square) {
+    priorColor = getComputedStyle(square).backgroundColor;
+    alpha = parseFloat(priorColor.split(",")[3]) + .1;
+    if (alpha < 1) {
+        square.style.backgroundColor = getRandomColor(alpha);
+    } else {
+        square.style.backgroundColor = getRandomColor(1);
+    }
 }
 
 function getUserInput(){
@@ -51,6 +68,25 @@ function changeGridSize (){
     });
 }
 
+function getRandomColor(alpha) {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b}, ${alpha})`
+}
+
+function getColorMode() {
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach(button => button.addEventListener("click", function(){
+        colorMode = this.className;
+        setUpGrid();
+        console.log(colorMode)
+    }))
+}
+
+let colorMode = "black"
 let gridSize = 16;
+
 setUpGrid();
+getColorMode();
 changeGridSize();
